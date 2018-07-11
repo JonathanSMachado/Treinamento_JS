8b6c2e863b841d2b28c9bcde732d88fa532130a3
Jonathan S. Machado <jonathansmachado@gmail.com>
2018-07-10 21:38:17 -0300

    formularios

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('Parabéns')
+    resp.send('Parabéns. Usuário incluído')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('Parabéns. Usuário alterado')
 })
 
 app.listen(3003)

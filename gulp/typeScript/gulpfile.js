a25903a812945dbb1c935ffde5b1254296aba085
Jonathan S. Machado <jonathansmachado@gmail.com>
2018-10-26 23:36:48 -0300

    gulp-spa

--- gulp/typeScript/gulpfile.js
+++ gulp/typeScript/gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
 
 const tsProjetc = ts.createProject('tsconfig.json')
 
 gulp.task('default', () => {
     return tsProjetc.src()
         .pipe(tsProjetc())
+        .pipe(uglify())
+        .pipe(concat('typescript.min.js'))
         .pipe(gulp.dest('build'))
 })

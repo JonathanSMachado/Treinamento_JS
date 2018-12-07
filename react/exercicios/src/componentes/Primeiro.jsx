2b32344461d743dbc8401efc113d9f3387e072ae
Jonathan S. Machado <jonathansmachado@gmail.com>
2018-12-07 00:26:19 -0200

    react primeiro componente

---
+++ react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => {
+    return <div>
+                <h1>Primeiro Componente React</h1>
+                <p>Muito legal criar interfaces reativas</p>
+            </div>
+}
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Iniciando com React</h1>
-ReactDOM.render(element, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro'
+
+// const element = <h1>Iniciando com React</h1>
+ReactDOM.render(<Primeiro />, document.getElementById('root'))

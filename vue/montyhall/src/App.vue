702b6e1609b69d904abcf99de22cbdbfe51ae357
Jonathan S. Machado <jonathansmachado@gmail.com>
2019-01-12 00:34:54 -0200

    Monty Hall Problem in Vue and React

---
+++ vue/montyhall/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    body {
+        box-sizing: border-box;
+        font-family: 'Montserrat', sans-serif;
+        background: linear-gradient(to right, #8e0e00, #1f1c18);
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        border: 5px solid #0003;
+        padding: 20px;
+        margin-bottom: 20px;
+        color: #fff9;
+    }
+</style>

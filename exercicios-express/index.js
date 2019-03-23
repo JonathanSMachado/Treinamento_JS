2e8be166ac2d58e959cfe2f8985c380693614a3e
Jonathan S. Machado <jonathansmachado@gmail.com>
2019-03-22 23:45:54 -0300

    continuação express

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express()
-const saudacao = require('./saudacaoMid')
+const bodyParser = require('body-parser')
 
+const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+require('./api/produto')(app, 'Com param!')
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
 app.use(saudacao('Jonathan'))
 
 app.use('/api', (req, res, next) => {
     console.log('Antes...')
     next()
+})
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo}, ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    // let corpo = ''
+    // req.on('data', function(parte) {
+    //     corpo += parte
+    // })
+
+    // req.on('end', function(){
+    //     res.send(corpo)
+    // })
+    res.send(req.body)
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
 app.get('/api', (req, res, next) => {
